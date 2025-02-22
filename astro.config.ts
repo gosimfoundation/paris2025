2586ef005e0189a8a1b4cb1914ea3f24c14fc2be
Edward Tan <guofoo@gmail.com>
2025-02-21 23:35:24 -0800

    update paths

--- astro.config.ts
+++ astro.config.ts
@@ -22,9 +22,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://gosimfoundation.github.io/paris2025',
-  base: '/paris2025/',
-
+  site: 'https://gosimfoundation.github.io/',
+  base: '/paris2025',
   output: 'static',
 
   integrations: [
